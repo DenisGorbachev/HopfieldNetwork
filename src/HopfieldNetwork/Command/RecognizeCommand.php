f82ea29cb4c7fd0f18c49e39c8b1596c208b37dc
Денис Горбачев <Denis.Gorbachev@trueoffice.ru>
2012-05-31 00:51:05 +0400

    Alpha version

--- src/HopfieldNetwork/Command/RecognizeCommand.php
+++ src/HopfieldNetwork/Command/RecognizeCommand.php
@@ -14,11 +14,91 @@ class RecognizeCommand extends Command
         $this
             ->setName('recognize')
             ->setDescription('Recognize the pattern')
+            ->addOption('seed', 's', InputOption::VALUE_REQUIRED, 'A number to seed the mt_rand generator', mt_rand())
             ->addArgument('patterns-file', InputArgument::REQUIRED, 'A file containing patterns this network is going to recognize')
-            ->addArgument('input-file', InputArgument::REQUIRED, 'A file containing data this network is going to recognize as pattern')
+            ->addArgument('inputs-file', InputArgument::REQUIRED, 'A file containing data this network is going to recognize as patterns')
     ;}
 
     protected function execute(InputInterface $input, OutputInterface $output) {
-        $output->writeln('hi');
+        $patterns = $this->readMatricesAsVectors($input->getArgument('patterns-file'));
+        $inputs = $this->readMatricesAsVectors($input->getArgument('inputs-file'));
+        $arity = count($patterns[0]);
+        $weights = array_fill(0, $arity, array_fill(0, $arity, 0));
+        for ($x = 0; $x < $arity; $x++) {
+            for ($y = $x; $y < $arity; $y++) {
+                if ($x != $y) { // otherwise it's already 0
+                    $sum = 0;
+                    foreach ($patterns as $pattern) {
+                        $sum += (2*$pattern[$x] - 1)*(2*$pattern[$y] - 1);
+                    }
+                    $weights[$x][$y] = $sum;
+                    $weights[$y][$x] = $sum;
+                }
+            }
+        }
+
+        $output->writeln('Patterns matrix:');
+        $this->printMatrix($patterns);
+        $output->writeln('');
+
+        $output->writeln('Weigth matrix:');
+        $this->printMatrix($weights);
+        $output->writeln('');
+
+        mt_srand($input->getOption('seed'));
+
+        foreach ($inputs as $patternToBe) {
+            $output->writeln('Recognizing');
+            $this->printMatrix(array($patternToBe));
+            do {
+                $updated = false;
+                $indexes = range(0, $arity-1);
+                for ($n = 0; $n < $arity; $n++) {
+                    $relativeIndex = mt_rand(0, count($indexes)-1);
+                    $index = $indexes[$relativeIndex];
+                    unset($indexes[$relativeIndex]);
+                    sort($indexes);
+                    $oldValue = $patternToBe[$index];
+                    $sum = 0;
+                    foreach ($weights[$index] as $j=>$weight) {
+                        $sum += $patternToBe[$j]*$weight;
+                    }
+                    $patternToBe[$index] = ($sum >= 0)? 1 : 0;
+                    $updated = $oldValue != $patternToBe[$index];
+                }
+            } while ($updated);
+            $output->writeln('as');
+            $this->printMatrix(array($patternToBe));
+            $output->writeln('');
+        }
+    }
+
+    protected function readMatricesAsVectors($filename)
+    {
+        $vectors = array(array());
+        $fp = fopen($filename, 'r');
+        while ($line = fgets($fp)) {
+            $matches = array();
+            preg_match_all('/1|0/', $line, $matches, PREG_PATTERN_ORDER);
+            $matches = $matches[0];
+            if (empty($matches)) {
+                $vectors[] = array();
+            } else {
+                $vectors[count($vectors) - 1] = array_merge($vectors[count($vectors) - 1], $matches);
+            }
+        }
+        if (empty($vectors[count($vectors)-1])) {
+            unset($vectors[count($vectors)-1]);
+        }
+        return $vectors;
+    }
+
+    public function printMatrix($matrix) {
+        foreach ($matrix as $row) {
+            foreach ($row as $value) {
+                echo sprintf('%2d', $value).' ';
+            }
+            echo PHP_EOL;
+        }
     }
 }
